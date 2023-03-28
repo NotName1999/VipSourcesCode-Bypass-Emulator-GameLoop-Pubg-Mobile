21557e658f08dd689c08f1db64b4f5af02299b07
NotName1999 <128921978+NotName1999@users.noreply.github.com>
2023-03-28 13:32:29 +0700

    Delete Memory.h

--- Memory.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "pch.h"
-int getProcId();
-int getAowProcId();
-bool patcher(long addr, BYTE write[], SIZE_T sizee);
-int SINGLEAOBSCAN2(BYTE BypaRep[], SIZE_T size);
-int SINGLEAOBSCAN3(BYTE BypaRep[], SIZE_T size);
-int SINGLEAOBSCAN(BYTE BypaRep[], SIZE_T size);
-BOOL MemSearch(BYTE* bSearchData, int nSearchSize, DWORD_PTR dwStartAddr, DWORD_PTR dwEndAddr, BOOL bIsCurrProcess, int iSearchMode, std::vector<DWORD_PTR>& vRet);
